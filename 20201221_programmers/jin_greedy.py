812607f3089d3228e844276508e97dca88c791db
IncredibleMonkey <42059680+IncredibleMonkey@users.noreply.github.com>
2020-12-22 15:37:22 +0900

    큰 수 만들기

---
+++ 20201221_programmers/jin_greedy.py
@@ -0,0 +1,26 @@
+import sys
+from collections import deque
+
+def solution(number, k):
+    number = deque(number)
+    stack = deque()
+
+    for comp in number:
+        while stack and stack[-1] < comp and k > 0:
+            deque.pop(stack)
+            k -= 1
+        else:
+            stack.append(comp)
+
+    if k > 0:    # 반례 5321
+        stack = list(stack)
+        stack = stack[:len(stack)-k]
+
+    ans = ''
+    return ''.join(stack)
+
+
+if __name__ == '__main__':
+    number = "5321"
+    k = 2
+    print(solution(number, k))
