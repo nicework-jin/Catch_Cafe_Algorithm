3f5081230fe54c3d291019265bffa485c07c414a
IncredibleMonkey <42059680+IncredibleMonkey@users.noreply.github.com>
2020-12-27 00:50:27 +0900

    가장 큰 수

--- 20201221_programmers/jin_greedy.py
+++ 20201221_programmers/jin_greedy.py
@@ -1,22 +1,11 @@
 import sys
-
-def solution(number, k):
-    number = list(number)
-    stack = []
-    for comp in number:
-        while stack and stack[-1] < comp and k > 0:
-            stack.pop(-1)
-            k -= 1
-        else:
-            stack.append(comp)
-
-    if k > 0:    # 반례 5321
-        stack = list(stack)
-        stack = stack[:len(stack)-k]
-    return ''.join(stack)
+def solution(numbers):
+    numbers = list(map(str, numbers))    
+    numbers = sorted(numbers, key=lambda x: x*4, reverse=False)    # 문제 조건에서 numbers 원소는 1000 이하이므로, 최소 4자리가 되도록 함.
+    return str(int(''.join(numbers)))
 
 if __name__ == '__main__':
-    number = "5321"
-    k = 2
-    print(solution(number, k))
-    
+    numbers = [61, 10, 2]
+    print(solution(numbers))
+
+
