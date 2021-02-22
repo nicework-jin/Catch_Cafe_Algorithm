9b5f5520d32b7a5f9ae21aabb38bbf006a31a513
NiceWork-Jin <42059680+NiceWork-Jin@users.noreply.github.com>
2021-02-22 20:41:02 +0900

    위장

---
+++ 20210222_programmers/hash.py
@@ -0,0 +1,10 @@
+def solution(clothes):
+    types = {}
+    for v, k in clothes:
+        types[k] = types.get(k, 0) + 1
+    tot = 0
+
+    cnt = 1
+    for _, v in types.items():
+        cnt *= v+1
+    return cnt-1
